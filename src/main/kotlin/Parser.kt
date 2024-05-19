bf15f301dec5a6937cc9ec6c2277b592bf9b055e
calt-laboratory <christoph_alt@posteo.de>
2024-05-19 14:59:39 +0200

    feat(html parser): implemented getURLs method

--- src/main/kotlin/Parser.kt
+++ src/main/kotlin/Parser.kt
@@ -1,13 +1,19 @@
 package org.example
 
+// Group that matches the URL in the a-tag
+private const val URL_GROUP_IDX = 2
+
 class HTMLParser {
 
-    fun findURLs (htmlText: String) : Set<String> {
-        println(aTagRegexPattern.findAll(input = htmlText).first().groups[2]?.value)
-        return setOf()
-    }
+    private val aTagRegexPattern = Regex(pattern = "<a[^>]*href=(['\"])(http.+?)\\1")
 
-    companion object {
-        val aTagRegexPattern = Regex(pattern = "<a[^>]*href=(['\"])(http.+?)\\1")
+    /**
+     * Gets all URLs for a given html text string.
+     * @param htmlText: HTML text from a given website
+     */
+    fun getURLs(htmlText: String) : Set<String> {
+        return aTagRegexPattern.findAll(input = htmlText)
+            .mapNotNull { it.groups[URL_GROUP_IDX]?.value }
+            .toSet()
     }
 }
