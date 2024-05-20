6f433b376c9e991e3f01fe6a2849253b229e1910
calt-laboratory <christoph_alt@posteo.de>
2024-05-20 19:12:16 +0200

    feat(crawler): implemented getHpyerlinkQuantityStatistics

--- src/main/kotlin/Webcrawler.kt
+++ src/main/kotlin/Webcrawler.kt
@@ -10,20 +10,37 @@ class WebCrawler(private val seedURL: String) {
     fun run() {
         val seedHyperlink = htmlParser.buildHyperlink(url = seedURL)
         val linkQueue = ArrayDeque(elements = setOf(seedHyperlink))
+
+        val collectedRootHyperlinks: MutableSet<Hyperlink> = mutableSetOf()
         val collectedRootDomains: MutableSet<String> = mutableSetOf()
 
+
         while (linkQueue.isNotEmpty()) {
-            if (collectedRootDomains.count() > 10) break
+            if (collectedRootDomains.count() > 5) break
 
             val link = linkQueue.removeFirst()
             val htmlText = readTextFromURL(url = link.url) ?: continue
 
             val urls = htmlParser.getHyperlinks(htmlText = htmlText).toSet()
             urls.forEach { linkQueue.add(it) }
+
+            collectedRootHyperlinks.add(element = link)
             collectedRootDomains.add(element = link.rootDomain)
 
             println("Current Link: ${link.url}")
+            println("${linkQueue.count()} hyperlinks are Currently queued ...")
         }
+
+        val protocolStats = getHyperlinkQuantityStats(hyperlinks = collectedRootHyperlinks) { it.protocol }
+        val topLevelDomainStats = getHyperlinkQuantityStats(hyperlinks = collectedRootHyperlinks) { it.topLevelDomain }
+        val rootDomainStats = getHyperlinkQuantityStats(hyperlinks = collectedRootHyperlinks) { it.rootDomain}
+
+        println("\n")
+        println("+++++++++++++++++ Statistics +++++++++++++++++")
+        println("Protocol statistics: $protocolStats")
+        println("Top-level-domain statistics: $topLevelDomainStats")
+        println("Root domain statistics: $rootDomainStats")
+
     }
 
     private fun readTextFromURL(url: String) : String? {
@@ -34,5 +51,9 @@ class WebCrawler(private val seedURL: String) {
             println("Error message: ${exception.message}")
             null
         }
+    }
+
+    private fun getHyperlinkQuantityStats(hyperlinks: Set<Hyperlink>, key: (Hyperlink) -> Any) : Map<String, Int> {
+        return hyperlinks.groupBy { key(it).toString() }.mapValues { it.value.count() }
     }
 }
